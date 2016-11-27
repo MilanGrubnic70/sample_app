5f1a99fe770a22836e067fc6bc25c616f0de2ddf
Milan Grubnić <MilanG@aol.com>
2016-11-26 22:15:38 -0600

    Add Contact Us page

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -44,7 +44,7 @@ describe "Static pages" do
 
 	describe "About page" do
 
-		it "should have the content 'About us'" do
+		it "should have the content 'About Us'" do
 			visit '/static_pages/about'
 			page.should have_content('About Us')
 		end
@@ -61,5 +61,26 @@ describe "Static pages" do
 		end
 
 	end
+
+	describe "Contact page" do
+
+		it "should have the content 'Contact Us'" do
+			visit '/static_pages/contact'
+			page.should have_content('Contact Us')
+		end
+
+		it "should have the h1 'Contact Us'" do
+			visit '/static_pages/contact'
+			page.should have_selector('h1', :text => 'Contact')
+		end
+
+		it "should have the title 'Contact'" do
+				visit '/static_pages/contact'
+				page.should have_selector('title', 
+					:text => "Ruby on Rails Tutorial Sample App | Contact Us")
+		end
+
+	end
+
 
 end
