95c0c5629965d88ff5fcfe4555d7cd1ad0b82b82
Milan Grubnić <MilanG@aol.com>
2017-01-02 18:46:29 -0600

    Refactor User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,7 +14,8 @@ class User < ActiveRecord::Base
   attr_accessible :name, :email, :password, :password_confirmation
   has_secure_password	
 
-  before_save { |user| user.email = user.email.downcase }
+  # before_save { |user| user.email = user.email.downcase } 
+  before_save { email.downcase! } #refactored line above 
 
   validates :name,  presence: true, length: { maximum: 50 }
 
