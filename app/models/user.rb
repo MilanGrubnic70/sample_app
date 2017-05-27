d91e85e169d8d7d0b3cb43172116c1f832793286
Milan Grubnić <MilanG@aol.com>
2017-05-26 21:27:35 -0500

    Add micropost model

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,7 +12,8 @@
 
 class User < ActiveRecord::Base
   attr_accessible :name, :email, :password, :password_confirmation
-  has_secure_password	
+  has_secure_password
+  has_many :microposts	
 
   # before_save { |user| user.email = user.email.downcase } 
   before_save { email.downcase! } #refactored line above 
